47347ce44beedd0091996df79e61f952c2d84021
Lenka Smitalova <Lenka@dhcp-228-118.eduroam.muni.cz>
2017-05-25 13:10:18 +0200

    android configuration repaired for emulator

--- thesis-app/app/shared/config.ts
+++ thesis-app/app/shared/config.ts
@@ -3,8 +3,8 @@ import application = require("application");
 
 //static variables definition for the application
 export class Config {
-    public static BASE_URL:string = 'http://0.0.0.0:3000';
-    public static API_VERSION:string = 'api';
-
     public static IOS_APP: boolean = application.ios ? true : false;
+
+    public static BASE_URL:string = Config.IOS_APP ? 'http://0.0.0.0:3000' : 'http://10.0.2.2:3000';
+    public static API_VERSION:string = 'api';
 }
