786f9a36ced3af6bafd3720f0857b7b295141026
Lenka Smitalova <Lenka@dhcp-239-191.eduroam.muni.cz>
2017-06-05 16:03:35 +0200

    =thesis-app/app/shared/sdk directory removed

--- thesis-app/app/shared/sdk/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-export * from './User';
-export * from './Account';
-export * from './Repository';
-export * from './Game';
-export * from './Achievement';
-export * from './RepositoryContributor';
-export * from './BaseModels';
-export * from './FireLoopRef';
--- thesis-app/app/shared/sdk/services/custom/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-export * from './User';
-export * from './Account';
-export * from './Repository';
-export * from './Game';
-export * from './Achievement';
-export * from './RepositoryContributor';
-export * from './SDKModels';
-export * from './logger.service';
